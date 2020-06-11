cefd06ce9044f5f80731f317b32d93d1fb6786ff
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-06-11 13:36:57 +0200

    Delete program.cmd

--- program.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-color 0a
-cls
-
-:A
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto A
